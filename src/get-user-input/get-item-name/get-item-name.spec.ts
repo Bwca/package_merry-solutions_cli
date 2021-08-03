d38fd319813284930692bfa2fe32e73a69b3a4e6
Volodymyr Yepishev <i.m.bwca@gmail.com>
2021-08-03 07:56:34 +0300

    added test cases for item name for kebab, snake and Pascal cases

--- src/get-user-input/get-item-name/get-item-name.spec.ts
+++ src/get-user-input/get-item-name/get-item-name.spec.ts
@@ -1,13 +1,60 @@
 import { getItemName } from './get-item-name';
 
-describe('Tests for getFileName', () => {
-    it('Given Pascal argument should return file name without any dashes in PascalCase', () => {
+describe('Tests for getFileName PascalCase', () => {
+    it('kebab-case to PascalCase', () => {
         // Arrange
-        const itemName = 'my-cool-component';
-        const expectedName = 'MyCoolComponent';
+        const itemName = 'my-such-cool-component';
+        const expectedName = 'MySuchCoolComponent';
 
         // Act
         const fileName = getItemName(itemName, 'PascalCase');
+
+        // Assert
+        expect(fileName).toEqual(expectedName);
+    });
+    it('snake_case to PascalCase', () => {
+        // Arrange
+        const itemName = 'my_such_cool_component';
+        const expectedName = 'MySuchCoolComponent';
+
+        // Act
+        const fileName = getItemName(itemName, 'PascalCase');
+
+        // Assert
+        expect(fileName).toEqual(expectedName);
+    });
+});
+
+describe('Tests for getFileName camelCase', () => {
+    it('kebab-case to camelCase', () => {
+        // Arrange
+        const itemName = 'my-such-cool-component';
+        const expectedName = 'mySuchCoolComponent';
+
+        // Act
+        const fileName = getItemName(itemName, 'camelCase');
+
+        // Assert
+        expect(fileName).toEqual(expectedName);
+    });
+    it('PascalCase to camelCase', () => {
+        // Arrange
+        const itemName = 'MySuchCoolComponent';
+        const expectedName = 'mySuchCoolComponent';
+
+        // Act
+        const fileName = getItemName(itemName, 'camelCase');
+
+        // Assert
+        expect(fileName).toEqual(expectedName);
+    });
+    it('snake_case to camelCase', () => {
+        // Arrange
+        const itemName = 'my_such_cool_component';
+        const expectedName = 'mySuchCoolComponent';
+
+        // Act
+        const fileName = getItemName(itemName, 'camelCase');
 
         // Assert
         expect(fileName).toEqual(expectedName);
--- src/get-user-input/get-item-name/get-item-name.ts
+++ src/get-user-input/get-item-name/get-item-name.ts
@@ -3,13 +3,13 @@ import { FileNameCase } from '../../shared/constants';
 export function getItemName(itemName: string, fileNameCase: FileNameCase): string {
     switch (fileNameCase) {
         case 'camelCase': {
-            return itemName.replace(/(-(\w)|(^\w))/g, (_, letterAfterDash, firstLetter) => {
-                return ((firstLetter?.toLowerCase()) || letterAfterDash?.toUpperCase());
+            return itemName.replace(/(^\w)|([-_](\w))/g, (_, firstLetter, __, letterAfterDash) => {
+                return firstLetter?.toLowerCase() || letterAfterDash?.toUpperCase();
             });
         }
 
         case 'PascalCase': {
-            return itemName.replace(/(-(\w)|(^\w))/g, (_, letterAfterDash, firstLetter) => {
+            return itemName.replace(/([-_](\w)|(^\w))/g, (_, letterAfterDash, firstLetter) => {
                 return (firstLetter || letterAfterDash).toUpperCase();
             });
         }
